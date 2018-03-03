66b1d03da34d34f9cb5aa5da79794f43f2ef71bc
Mark Wissink <mcw33@cs-ssh.cs.calvin.edu>
2018-03-03 16:47:20 -0500

    defined find function in Path

--- src/commandline.hxx
+++ src/commandline.hxx
@@ -1,14 +1,17 @@
+#pragma once
 #include <iostream>
 #include <vector>
 #include <cstring>
 
 class CommandLine {
-  int argc;
-  vector<char*> argv;
-  CommandLine(istream& in);
-  char* getCommand() const;
-  int getArgCount() const;
-  char** getArgVector() const;
-  char* getArgVector(int i) const;
-  bool noAmpersand() const;
-}
+  public:
+    CommandLine(istream& in);
+    char* getCommand() const;
+    int getArgCount() const;
+    char** getArgVector() const;
+    char* getArgVector(int i) const;
+    bool noAmpersand() const;
+  private:
+    int argc;
+    std::vector<char*> argv;
+};
--- src/path.cxx
+++ src/path.cxx
@@ -1,21 +1,57 @@
 #include "path.hxx"
+
+#include <sys/types.h>
+#include <errno.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 #include <iostream>
 
 Path::Path() {
   char *path = getenv("PATH");
   char *token;
+  // Tokenize the string and store it in dirs
   while ((token = strtok_r(path, ":", &path))) {
     dirs.push_back(token);
   }
-
-  for (size_t n = 0; n < dirs.size(); n++)
-    std::cout << dirs[n] << std::endl;
 }
 
-//int Path::find(const std::string& program) const {};
+int Path::find(const std::string& program) const {
+  DIR *dir;
+  struct dirent *dp;
+  
+  // Loop through all the directories in our path
+  for (size_t i = 0; i < dirs.size(); i++) {
+    // Open the directory
+    if ((dir = opendir(dirs[i]) == NULL) {
+      perror("Cannot open %s", dirs[i]);
+      return -1;
+    } 
+    
+    // Run until we close the directory
+    while (dir) {
+      if ((dp = readdir(dir)) != NULL) {
+        // Compare the name in the dirent structure to program
+        if (dp->d_name == program) {
+          closedir(dir);
+          // Return the index of the directory
+          return i;
+        }
+      } else {
+        if (errno == 0) {
+          // File was not found
+          closedir(dir);
+          return -1;
+        }
+        // There was an error
+        closedir(dir);
+        perror("Read error");
+        return -1;
+      }
+    }
+  } 
+};
 
 std::string Path::getDirectory(int i) const {
   return dirs[i];
