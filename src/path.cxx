363ee28498f4a01c409c4a155ce947fa036d0deb
Mark Wissink <mwissink.b@gmail.com>
2018-03-05 00:09:16 -0500

    updated path find method

--- src/path.cxx
+++ src/path.cxx
@@ -38,32 +38,17 @@ int Path::find(const std::string& program) const {
   // Loop through all the directories in our path
   for (size_t i = 0; i < dirs.size(); i++) {
     // Open the directory
-    if ((dir = opendir(dirs[i].c_str())) == NULL) {
-      perror("Cannot open directory");
-      return -1;
-    }
+    opendir(dirs[i].c_str());
 
     // Run until we close the directory
-    while (dir) {
-      if ((dp = readdir(dir)) != NULL) {
+    while (dp = readdir(dir)) {
         // Compare the name in the dirent structure to program
         if (dp->d_name == program) {
           closedir(dir);
-          // Return the index of the directory
           return i;
         }
-      } else {
-        if (errno == 0) {
-          // File was not found
-          closedir(dir);
-          return -1;
-        }
-        // There was an error
-        closedir(dir);
-        perror("Read error");
-        return -1;
-      }
     }
+    closedir(dir);
   }
   return -1;
 };
